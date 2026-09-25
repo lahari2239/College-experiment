b6166b28f7a9d9a049c1c12ed36dca9756c6f22c
lahari2239 <laharilahari845@gmail.com>
2026-09-25 11:58:06 +0530

    Create StringLiterals component with basic props

---
+++ S Literals.js
@@ -0,0 +1,10 @@
+impot React from "react";
+function StringLiterals(){constname="saritha";constage=21;
+                          return(
+                            <div>
+                            <h2>using StringLiterals</h2>
+                          <p>Hello,my name is{name} and i am {age}yearsold.</p>
+                            </div>
+                          );
+                         }
+export default StringLiterals
